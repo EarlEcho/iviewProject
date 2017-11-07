08555bee615f03d237862cbd8b3d3bcee63cdaa4
EarEcho <734952325@qq.com>
2017-11-07 10:41:09 +0800

    路由mode更改为history

--- src/main.js
+++ src/main.js
@@ -1,18 +1,37 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
 import VueRouter from 'vue-router';
-import router from './router/index.js'
+import Routers from './router';
+import App from './app.vue';
 import 'iview/dist/styles/iview.css';
+
+Vue.use(VueRouter);
 
 // 路由配置
 const RouterConfig = {
     mode: 'history',
-    routes: router
+    routes: Routers
 };
+const router = new VueRouter(RouterConfig);
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.title) {//如果设置标题，拦截后设置标题
+        document.title = to.meta.title
+    }
+    next()
+});
 /* eslint-disable no-new */
 new Vue({
-    router,
+    el: '#app',
+    router: router,
     render: h => h(App)
-}).$mount('#app')
+});
+
+
+
+
+
+
+
+
